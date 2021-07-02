e770c6a87551be8b5d0346b7d2cfc6a058b73973
Steph3nie <86668009+Steph3nie@users.noreply.github.com>
2021-07-01 21:40:13 -0400

    Set Up Game With Mechanics
    The background and main objects have been assembled in Unity. The paper basketball can be launched

---
+++ Assets/Scripts/PaperBall.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float releaseTime = .15f;
+
+    private bool isPressed = false;
+
+    private void Update()
+    {
+        if(isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+    }
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+        Debug.Log("Mouse Click");
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        Debug.Log("Mouse Released");
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
